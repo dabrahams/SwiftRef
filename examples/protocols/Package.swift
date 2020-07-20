0ca9621a3cd7f75603763f3f62b04070904f2106
Matt Rips <mrips@raklaw.com>
2020-07-20 10:17:22 -0700

    Wrapped Package.swift at 80.

--- examples/protocols/Package.swift
+++ examples/protocols/Package.swift
@@ -1,5 +1,6 @@
 // swift-tools-version:5.1
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// The swift-tools-version declares the minimum version of Swift required 
+// to build this package.
 
 import PackageDescription
 
